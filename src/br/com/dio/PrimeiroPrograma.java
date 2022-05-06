d34ee267c12ecedd695c02b340eef65961eabac0
Ana Claudia <anaclaudiammatos@hotmail.com>
2022-05-06 00:02:11 -0300

    criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,10 +6,42 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
 
-
         System.out.println(gato);
         /*int a = 5;
         int b = 3;
         System.out.println("Hello Word!" + (a+b));*/
+    }
+}
+class Livro {
+    private String nome;
+    private Integer numero;
+
+    public Livro(String nome, Integer numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numero=" + numero +
+                '}';
     }
 }
